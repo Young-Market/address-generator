df6061531d2aeee61b111f5bf0d5414937596b44
D-H-O-R-A <bicontoken@gmail.com>
2024-08-01 09:09:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var youngMarketConfig = {    
+    logLevel: "warning",
+    matcherAddress: "https://matcher.youngmarket.com",
+    networkByte: "Y".charCodeAt(0),
+    nodeAddress: "https://nodes.youngmarket.com",
+    requestLimit: 1000,
+    requestOffset: 0,
+    timeDiff: 0
+}
+
+var jsonData;
+
+function generateAddress(){
+    $('#divA,[onselect="saveAddress()"]').css("display","flex")
+    test = WavesAPI.create(youngMarketConfig )
+    var s = test.Seed.create()
+    $("#address").text(s.address)
+    $("#seeed").text(s.phrase)
+    $("#pk").text(s.keyPair.privateKey)
+    $("#puk").text(s.keyPair.publicKey)
+    $('[onclick="generateAddress()"]').css("display","none")
+    jsonData = s;
+}
+
+function saveAddress(){
+          // Converte o objeto JSON em uma string
+          const jsonString = JSON.stringify(jsonData);
+
+          // Cria um Blob com o conteúdo JSON e especifica o tipo de MIME
+          const blob = new Blob([jsonString], { type: 'application/json' });
+    
+          // Cria um link temporário para o Blob
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(blob);
+          link.download = 'address_young_market.json';
+          link.click();
+}
